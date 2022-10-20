0a2e8c736c2d3a87e96713fea03ff6a1cab565b2
Jephte Damas <jephtedamas@hotmail.ca>
2022-10-20 18:35:07 -0400

    Seconde partie

---
+++ main.js
@@ -0,0 +1,14 @@
+window.addEventListener("DOMContentLoaded", function () {
+
+    let elForm = document.querySelector("[data-js-form]"),
+	 	elBtn = document.querySelector("[data-js-btn]");
+
+
+		elBtn.addEventListener("click", function() {
+		
+			open("https://unity.com/fr/madewith/hollow-knight");
+		
+		
+		});
+
+});
